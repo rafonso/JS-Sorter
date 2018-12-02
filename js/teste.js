684f7eb3ade3c43f21dbf1672bd5f2c559829e33
Rafael Ubiratam Clemente Afonso <rafael.afonso@gmail.com>
2018-12-02 16:55:14 -0200

    Seleção do tipo de exibição.

--- js/teste.js
+++ js/teste.js
@@ -1,5 +1,16 @@
 "use strict";
 
+let tiposExibicao = new Map([
+    ["espectro", {
+        titulo: "Espectro",
+        gerador: (eventoInicial, cores) => new Espectro(eventoInicial, cores)
+    }],
+    ["pontos", {
+        titulo: "Pontos",
+        gerador: (eventoInicial, cores) => new CanvasPontos(eventoInicial, cores)
+    }]
+]);
+
 $(document).ready(
     function () {
         let componentsController = new ComponentsController();
@@ -11,25 +22,30 @@ class ComponentsController {
     constructor() {
         this.valores = [];
         this.cores = [];
-        this.espectro = new Espectro(new SortEvent(SortEvent.IDLE, this.valores), this.cores);
+        // this.espectro = new Espectro(new SortEvent(SortEvent.IDLE, this.valores), this.cores);
         this.geradorSequencia = new GeradorSequencia();
         this.dadosForamOrdenados = false;
+        /**
+         * @property 
+         * @function
+         */
+        this.geradorExibicao = null;
+        this.exibicao = null;
 
         this.areaControles = $("#controles");
 
         this.selQuantidade = $("#quantidade");
-        this.selExibicao = $("#exibicao");
+        this.selExibicao = $("#exibicao").html(
+            "<option></option> " +
+            Array.from(tiposExibicao.keys()).map(tipo => `<option value='${tipo}'>${tiposExibicao.get(tipo).titulo}</option>`)
+        );
         this.selTipo = $("#tipo").html(
             "<option></option> " +
             Array.from(sorterType.keys()).map(type => `<option>${type}</option> `)
         );
-        this.selSequencia = $("#sequencia").html(() => {
-            let options = "";
-            for (let v in TipoSequencia) {
-                options += `<option>${TipoSequencia[v]}</option>`;
-            }
-            return options;
-        });
+        this.selSequencia = $("#sequencia").html(() =>
+            Object.keys(TipoSequencia).map(type => `<option>${TipoSequencia[type]}</option> `)
+        );
 
         this.btnAtivarSom = $("#ativarSom");
         this.btnAtivarConsole = $("#ativarConsole");
@@ -41,10 +57,29 @@ class ComponentsController {
         this.selTipo.change(function () {
             self.btnOrdenar.prop("disabled", (!!self.valores && !!self.selTipo.val() && !self.dadosForamOrdenados) ? null : "disabled");
         });
+        this.selExibicao.click(function () {
+            self._selecionarExibicao($(this).val());
+        });
 
         this.txtComparacoes = $("#comparacoes");
         this.txtTrocas = $("#trocas");
         this.txtTempo = $("#tempo");
+    }
+
+    /**
+     * @private
+     * @param {string} opcao 
+     */
+    _selecionarExibicao(opcao) {
+        $(".exibicao").hide();
+        if (!opcao) {
+            this.geradorExibicao = null;
+            this.btnGerarNumeros.attr("disabled", "disabled");
+        } else {
+            this.geradorExibicao = tiposExibicao.get(opcao).gerador;
+            this.btnGerarNumeros.removeAttr("disabled");
+            $(`#${opcao}`).show();
+        }
     }
 
     gerarValores() {
@@ -54,8 +89,7 @@ class ComponentsController {
         this.dadosForamOrdenados = false;
 
         let initialEvent = new SortEvent(SortEvent.IDLE, this.valores);
-        this.espectro = new Espectro(initialEvent, this.cores);
-        this.canvasPontos = new CanvasPontos(initialEvent, this.cores);
+        this.exibicao = this.geradorExibicao(initialEvent, this.cores);
         this.btnOrdenar.prop("disabled", (!!this.selTipo.val()) ? null : "disabled");
         this.txtComparacoes.val(null);
         this.txtTrocas.val(null);
@@ -67,8 +101,7 @@ class ComponentsController {
         let listeners = [
             this.contador,
             this,
-            this.espectro,
-            this.canvasPontos
+            this.exibicao
         ];
         if (this.btnAtivarConsole.is(":checked")) {
             listeners.push(new EventLogger());
